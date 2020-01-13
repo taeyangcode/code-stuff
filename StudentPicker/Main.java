a588e1ebf89f3214917d36a2e1cabc267f42ea15
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-01-12 23:58:58 -0800

    Create Main.java

---
+++ StudentPicker/Main.java
@@ -0,0 +1,12 @@
+package com.jetbrains;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Swing swingRun = new Swing();
+
+	    swingRun.frameSetup();
+	    swingRun.inputPanelSetup();
+	    swingRun.visibility();
+    }
+}
