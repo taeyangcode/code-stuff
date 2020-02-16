ac208f8f5aa7d5b54e01a4c2827094e01374a468
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-02-16 02:05:52 -0800

    Create Colors.java

---
+++ MomLoveCalendar/Colors.java
@@ -0,0 +1,69 @@
+package com.jetbrains;
+
+import java.awt.Color;
+
+public class Colors {
+
+    Color oneLove() {
+
+        return Color.decode("#190000");
+
+    }
+
+    Color twoLove() {
+
+        return Color.decode("#330000");
+
+    }
+
+    Color threeLove() {
+
+        return Color.decode("#4C0000");
+
+    }
+
+    Color fourLove() {
+
+        return Color.decode("#660000");
+
+    }
+
+    Color fiveLove() {
+
+        return Color.decode("#7F0000");
+
+    }
+
+    Color sixLove() {
+
+        return Color.decode("#990000");
+
+    }
+
+    Color sevenLove() {
+
+        return Color.decode("#B20000");
+
+    }
+
+    Color eightLove() {
+
+        return Color.decode("#CC0000");
+
+    }
+
+    Color nineLove() {
+
+        return Color.decode("#E50000");
+
+    }
+
+    Color tenLove() {
+
+        return Color.decode("#FF0000");
+
+    }
+
+
+
+}
