620be0067018fcb266e774977b8561c477c05a9b
태양 <56237265+taeyangcode@users.noreply.github.com>
2021-03-16 08:13:46 -0700

    Create hiddenword.java

---
+++ hiddenword.java
@@ -0,0 +1,40 @@
+/*
+Answer without recursion:
+*/
+
+public class HiddenWord {
+    private String answer, ans;
+    
+    public HiddenWord(String answer) {
+        this.answer = answer;
+        this.ans = answer;
+    }
+    
+    public String getHint(String guess) {
+        if(guess.length() == 0) return guess;
+        String ansFL = answer.substring(0, 1), guessFL = guess.substring(0, 1), newGuess = guess.substring(1);
+        answer = answer.substring(1);
+        return ansFL.equals(guessFL) ? guessFL + getHint(newGuess) : ans.contains(guessFL) ? "+" + getHint(newGuess) : "*" + getHint(newGuess);
+    }
+}
+
+/*
+Answer with recursion:
+*/
+
+public class HiddenWord {
+    private String answer;
+    
+    public HiddenWord2(String answer) {
+        this.answer = answer;
+    }
+    
+    public String getHint(String guess) {
+        String ans = "";
+        for(int i = 0; i < answer.length(); i++) {
+            String answerPos = answer.substring(i, i + 1), guessPos = guess.substring(i, i + 1);
+            ans += answerPos.equals(guessPos) ? answerPos : answer.contains(guessPos) ? "+" : "*";
+        }
+        return ans;
+    }
+}
